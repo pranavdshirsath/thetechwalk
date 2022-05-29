c1881c52dff32f74de609e271b902c6868a8af8f
unknown <jai.d.shirsath@gmail.com>
2022-05-29 16:34:54 +0530

    Second

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,7 +12,6 @@ const Header = () => {
     });
   }, []);
   
-
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
