18532103034d776bed1092606e1798b708ab675b
unknown <jai.d.shirsath@gmail.com>
2022-05-29 16:27:27 +0530

    First Commit

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { getCategories } from '../services';
+
+const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+  
+
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full inline-block border-blue-400 py-8">
+        <div className="md:float-left block">
+          <Link href="/">
+            <Image src="/logo.png" width={250} height={100} alt="logo for the tech walk- tech news and latest trends!" />
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
